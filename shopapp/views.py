f26f0c11e3fb2dcd46c5d0c05e20d0ae60bb7353
srstig <rayogutu36@gmail.com>
2025-02-18 13:38:27 +0300

    initial commit

---
+++ shopapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def images(request):
+    return render(request, 'gallary.html')
